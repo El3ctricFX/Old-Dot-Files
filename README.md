f0ac3b41b00bc685c3dfef3678aa05a948e3e6ba
Electric <173574107+El3ctricFX@users.noreply.github.com>
2024-11-29 17:43:09 -0500

    setup files

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 within this repo i have used gnu stow to link all my config files into 1 directory.
 
 s/o to [yawn3k](https://github.com/yawn3k?tab=repositories) for helping with hyprland, zsh, and GNU Stow
+
+# Setup files
+
+the files within /setup-files/ are in a testing stage which will more then likely replace your current config files that could break systems so becareful! 
---
+++ setup-files/Arch-setup.sh
@@ -0,0 +1,20 @@
+# installs Applications
+sudo pacman -S stow fastfetch rofi hyprland kitty waybar neovim hyprpaper waypaper starship git
+
+# Updates
+Sudo Pacman -Syu
+
+# Pulls from github
+git clone https://github.com/El3ctricFX/Dot-Files.git
+
+# Goes into directory
+cd Dot-Files
+
+# Replacing Files
+stow Fastfetch
+stow hypr
+stow kitty
+stow nvim
+stow rofi
+stow starship
+stow waybar
